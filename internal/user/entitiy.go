d0ccef53cf918a0578cd12ebfdba78b1f45282f0
mizanmahi <mizanmahi24@gmail.com>
2026-05-22 02:06:33 +0600

    refactor user entity and repository structure to improve validation and organization

--- internal/user/entitiy.go
+++ internal/user/entitiy.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name" validate:"required" gorm:"type:varchar(100);not null"`
-	Email    string `json:"email" validate:"required,email" gorm:"type:varchar(255);uniqueIndex;not nll"`
-	Password string `json:"password" validate:"required,min=6" gorm:"type:varchar(100);not null"`
+	Name     string `json:"name"  gorm:"type:varchar(100);not null"`
+	Email    string `json:"email"  gorm:"type:varchar(255);uniqueIndex;not nll"`
+	Password string `json:"password"  gorm:"type:varchar(100);not null"`
 }
--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,1 +1,11 @@
 package user
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	CreateUser(user *User) error
+}
+
+type repository struct {
+	db *gorm.DB
+}
