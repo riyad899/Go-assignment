7fc0b7782d68714744fed3814afc29c631d03d78
mizanmahi <mizanmahi24@gmail.com>
2026-05-22 02:35:38 +0600

    refactor user repository and service structure to enhance user creation logic and error handling

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,6 +1,12 @@
 package user
 
-import "gorm.io/gorm"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+var ErrorAlreadyExist = errors.New("user with this email already exist")
 
 type Repository interface {
 	CreateUser(user *User) error
@@ -8,4 +14,26 @@ type Repository interface {
 
 type repository struct {
 	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) Repository {
+
+	return &repository{
+		db: db,
+	}
+}
+
+func (r repository) CreateUser(user *User) error {
+
+	result := r.db.Create(user)
+	if result.Error != nil {
+
+		if errors.Is(result.Error, gorm.ErrDuplicatedKey) {
+			return ErrorAlreadyExist
+		}
+
+		return result.Error
+	}
+
+	return nil
 }
--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,15 @@
 package user
+
+type service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) *service {
+	return &service{
+		repo: repo,
+	}
+}
+
+func (s *service) CreateUser(user *User) error {
+	return s.repo.CreateUser(user)
+}
