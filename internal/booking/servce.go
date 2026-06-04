42a89738dc2b03380cfd183f04c8459ffb0b77ae
mizanmahi <mizanmahi24@gmail.com>
2026-06-04 22:38:40 +0600

    refactor: remove unused event repository from booking service

--- internal/booking/servce.go
+++ internal/booking/servce.go
@@ -1,12 +1,7 @@
 package booking
 
-import (
-	"gotickets/internal/event"
-)
-
 type service struct {
 	bookingRepo Repository
-	eventRepo   event.Repository
 }
 
 func NewService(bookingRepo Repository) *service {
